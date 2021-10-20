dfcb87a77fb5bf3c11552a3a7317d3aab3c3d90d
thiagowaib <thiagowaib@gmail.com>
2021-10-20 10:57:56 -0300

    [feat] Tema

---
+++ assets/Theme.js
@@ -0,0 +1,51 @@
+import { extendTheme } from "native-base";
+
+const Theme = extendTheme({
+    // fontConfig: {
+    //     Barlow: {
+    //         100: {
+    //             normal: 'Barlow-Light',
+    //             italic: 'Barlow-LightItalic',
+    //         },
+    //         200: {
+    //             normal: 'Barlow-Light',
+    //             italic: 'Barlow-LightItalic',
+    //         },
+    //         300: {
+    //             normal: 'Barlow-Light',
+    //             italic: 'Barlow-LightItalic',
+    //         },
+    //         400: {
+    //             normal: 'Barlow-Regular',
+    //             italic: 'Barlow-Italic',
+    //         },
+    //         500: {
+    //             normal: 'Barlow-Medium',
+    //             italic: 'Barlow-MediumItalic',
+    //         },
+    //         600: {
+    //             normal: 'Barlow-Medium',
+    //             italic: 'Barlow-MediumItalic',
+    //         },
+    //         700: {
+    //             normal: 'Barlow-Bold',
+    //             italic: 'Barlow-BoldItalic',
+    //         },
+    //         800: {
+    //             normal: 'Barlow-Bold',
+    //             italic: 'Barlow-BoldItalic',
+    //         },
+    //         900: {
+    //             normal: 'Barlow-Bold',
+    //             italic: 'Barlow-BoldItalic',
+    //         },
+    //     }
+    // },
+    // fonts: {
+    //     heading: 'Barlow',
+    //     body: 'Barlow',
+    //     mono: 'Barlow',
+    // }
+})
+
+export default Theme
