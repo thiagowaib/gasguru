4e0d0f37d4e307f91d5c19be40ae28ce3d2949dc
thiagowaib <thiagowaib@gmail.com>
2021-10-20 10:58:51 -0300

    [fix] Splashscreen [feat] StatusBar

--- App.js
+++ App.js
@@ -2,20 +2,28 @@
 import React, { useState } from "react";
 import AppLoading from "expo-app-loading";
 import { Asset } from "expo-asset";
+import { StatusBar } from "expo-status-bar";
 
 import { NativeBaseProvider } from "native-base";
 import MainStack from "./src/navigators/MainStack";
+import Theme from './assets/Theme'
 
 export default function App() {
   const [isReady, setIsReady] = useState(false);
 
   const _cacheResourcesAsync = async () => {
-    const images = [require("../assets/splash/splashScreen.png")];
+    const images = [require("./assets/splash.png")];
     const cacheImages = images.map((image) => {
       return Asset.fromModule(image).downloadAsync();
     });
     return Promise.all(cacheImages);
   };
+
+  const config = {
+    dependencies: {
+      'linear-gradient': require('expo-linear-gradient').LinearGradient
+    }
+  }
   
   return isReady === false ? (
     <AppLoading
@@ -24,8 +32,9 @@ export default function App() {
       onError={console.warn}
     />
   ) : (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={Theme} config={config}>
       <MainStack />
+      <StatusBar backgroundColor="#1D191C" style="light"/>
     </NativeBaseProvider>
   );
 }
